064c8a24956c20c7f04fc0c00278b0a01d9d9fd3
Nasheed Ibrahim <35289342+natsudotmv@users.noreply.github.com>
2025-11-21 20:38:42 +0500

    AppDbContext andporstgresd database config added

--- relayowl-back-end/src/Relayowl.Infrastructure/Data/AppDbContext.cs
+++ relayowl-back-end/src/Relayowl.Infrastructure/Data/AppDbContext.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Relayowl.Infrastructure.Data;
 
-public class AppDbContext
+public class AppDbContext : DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
 }
